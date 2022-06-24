3af4f100f0634575b86e7c26cd2a0f7713f25e0b
hakktastic <hakktastic@gmail.com>
2022-06-24 14:42:01 +0200

    HAK-40 create service object
    to manage Customer entities

---
+++ src/main/java/nl/hakktastic/leaseacarapi/service/CustomerService.java
@@ -0,0 +1,73 @@
+package nl.hakktastic.leaseacarapi.service;
+
+import nl.hakktastic.leaseacarapi.entity.Customer;
+import nl.hakktastic.leaseacarapi.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service object for managing {@link nl.hakktastic.leaseacarapi.entity.Customer} entities.
+ */
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    /**
+     * Create a {@link Customer} entity.
+     *
+     * @param customer {@link Customer}
+     * @return Returns the customer created as an {@link Optional}
+     */
+    public Optional<Customer> createCustomer(Customer customer) {
+
+        return Optional.of(repository.save(customer));
+    }
+
+    /**
+     * Delete a {@link Customer} entity.
+     *
+     * @param id ID of the customer to be deleted
+     */
+    public void deleteCustomer(int id) {
+
+        var customerEntity = repository.getReferenceById(id);
+        repository.delete(customerEntity);
+    }
+
+    /**
+     * Get all {@link Customer} entities from the repository.
+     *
+     * @return Returns a {@link List} with found {@link Customer} entities
+     */
+    public List<Customer> getAllCustomers() {
+
+        return repository.findAll();
+    }
+
+    /**
+     * Find a single {@link Customer} entity with provided ID.
+     *
+     * @param id ID of the car to be returned
+     * @return Returns an {@link Optional} containing the {@link Customer} entity
+     */
+    public Optional<Customer> getSingleCustomer(int id) {
+
+        return repository.findById(id);
+    }
+
+    /**
+     * Find a single {@link Customer} entity with provided name.
+     *
+     * @param name of the customer to be returned
+     * @return Returns an {@link Optional} containing the {@link Customer} entity
+     */
+    public Optional<Customer> getSingleCustomer(String name) {
+
+        return repository.findByName(name);
+    }
+}
